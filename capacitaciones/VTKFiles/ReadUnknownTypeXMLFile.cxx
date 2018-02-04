93df6da1a507489611db04bdc4a788342f1928da
stephanieDominguez <s.dominguez@javeriana.edu.co>
2018-02-04 11:36:12 -0500

    capacitacion de vtk, con xml y archivos de texto.

---
+++ capacitaciones/VTKFiles/ReadUnknownTypeXMLFile.cxx
@@ -0,0 +1,69 @@
+#include <vtkSmartPointer.h>
+#include <vtkXMLGenericDataObjectReader.h>
+
+#include <vtkPolyData.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkDataSetMapper.h>
+#include <vtkActor.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkNamedColors.h>
+
+int main(int argc, char *argv[])
+{
+  // Ensure a filename was specified
+  if(argc < 2)
+  {
+    std::cout << "Usage: " << argv[0] << " InputFilename" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Get all data from the file
+  vtkSmartPointer<vtkXMLGenericDataObjectReader> reader =
+    vtkSmartPointer<vtkXMLGenericDataObjectReader>::New();
+  reader->SetFileName(argv[1]);
+  reader->Update();
+
+  // All of the standard data types can be checked and obtained like this:
+  if(vtkPolyData::SafeDownCast(reader->GetOutput()))
+  {
+    std::cout << "File is a polydata" << std::endl;
+  }
+  else if(vtkUnstructuredGrid::SafeDownCast(reader->GetOutput()))
+  {
+    std::cout << "File is an unstructured grid" << std::endl;
+  }
+
+  // Visualize
+  vtkSmartPointer<vtkNamedColors> colors =
+    vtkSmartPointer<vtkNamedColors>::New();
+  vtkSmartPointer<vtkDataSetMapper> mapper =
+    vtkSmartPointer<vtkDataSetMapper>::New();
+  mapper->SetInputConnection(reader->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> actor =
+    vtkSmartPointer<vtkActor>::New();
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetColor(colors->GetColor3d("Moccasin").GetData());
+
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  renderWindow->AddRenderer(renderer);
+  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  renderer->AddActor(actor);
+  renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
+  renderWindow->SetSize(640, 480);
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
+
